aecdaa53493bee563a4b3f777dc8f60255a57bb5
Samuel Borges <Herograme@gmail.com>
2025-01-29 20:37:25 -0300

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  import Header from './components/Header.vue'
+  import HeroSection from './components/HeroSection.vue'
+</script>
+
+<template>
+  <Header />
+  <HeroSection />
+</template>
+
+
